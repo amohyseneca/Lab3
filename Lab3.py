177d3a60583866dd9f22b95504418379fcb70cf1
amohyseneca <amohy@myseneca.ca>
2023-02-15 16:29:00 -0500

    initial commit

--- Lab3.py
+++ Lab3.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
-def writeToFile(fileName, text)
-  file open(fileName, 'w')
+def writeToFile(fileName, text):
+  file.open(fileName, 'w')
   file.write(text + '\n')
   file.close
-  
+
+def helloWorld():
+  print("Hello World")
+
+
+
 if __name__ == '__main__':
+  helloWorld()
   writeToFile(test.txt, 'Hello.')
