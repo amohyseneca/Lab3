a858ccfe966b838fdb26c31a0bb816e6186ee9bd
amohyseneca <125282579+amohyseneca@users.noreply.github.com>
2023-02-15 14:52:37 -0500

    Create Lab3.py

---
+++ Lab3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def writeToFile(fileName, text)
+  file open(fileName, 'w')
+  file.write(text + '\n')
+  file.close
+  
+if __name__ == '__main__':
+  writeToFile(test.txt, 'Hello.')
